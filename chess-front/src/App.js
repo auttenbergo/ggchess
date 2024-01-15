52d3770bff930e8d4c45feca241f9953ec4fd5ff
gvaro <g.gvaramia@lider-bet.com>
2024-01-15 19:07:28 +0400

    Add kochli Promotion [front]

--- chess-front/src/App.js
+++ chess-front/src/App.js
@@ -6,6 +6,7 @@ import React from "react";
 
 function App() {
   const [boardState, setBoardState] = useState({});
+  const [lastClickedPieceData, setLastClickedPieceData] = useState({});
 
   useState(() => {
     axios
@@ -18,9 +19,51 @@ function App() {
       });
   }, []);
 
+  const onPromotionPieceSelect = (promotedPiece) => {
+    console.log("onPromotionPieceSelect", promotedPiece);
+
+    const newPiece = promotedPiece[1];
+
+    axios
+      .post("http://localhost:8180/chess/move", {
+        from: lastClickedPieceData.sourceSquare,
+        to: lastClickedPieceData.targetSquare,
+        additionalProperties: {
+          promoteTo: newPiece,
+        },
+      })
+      .then((response) => {
+        console.log("Move response:", response.data);
+        setBoardState(response.data);
+      })
+      .catch((error) => {
+        console.log("Move error:", error.message);
+      });
+  };
+
+  const onPromotionCheck = (sourceSquare, targetSquare, piece) => {
+    setLastClickedPieceData({
+      sourceSquare,
+      targetSquare,
+      piece,
+    });
+
+    console.log("On Promotion Check", sourceSquare, targetSquare, piece);
+
+    if (targetSquare[1] != 8 && targetSquare[1] != 1) {
+      console.log("Returned false 1");
+      return false;
+    }
+    if (piece[1].toLowerCase() != "p") {
+      console.log("Returned false 2", piece.toLowerCase());
+      return false;
+    }
+    console.log("Returned true");
+    return true;
+  };
+
   const onPieceDrop = (sourceSquare, targetSquare, piece) => {
     const player = piece[0];
-
     console.log("onPieceDrop:", sourceSquare, targetSquare, piece[1], player);
 
     axios
@@ -43,9 +86,9 @@ function App() {
     <div className="App">
       <Chessboard
         position={boardState}
-        onPieceDrop={(sourceSquare, targetSquare, piece) =>
-          onPieceDrop(sourceSquare, targetSquare, piece)
-        }
+        onPieceDrop={onPieceDrop}
+        onPromotionPieceSelect={onPromotionPieceSelect}
+        onPromotionCheck={onPromotionCheck}
         id="BasicBoard"
       />
     </div>
